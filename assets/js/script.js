3f16d8ff344a4b0b72cf6549a95d44cb8e6823f8
ezzeldenWork <ezzelden.work@gmail.com>
2021-08-01 10:36:28 +0200

    new -v

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,77 @@
 $(document).ready(function () {
-
-    $('.data-label .item-box').hover(function () {
-        // debugger;
-        var img_src = $(this).children(".img-box").children('img').attr("src"),
-            box_title = $(this).children(".item-name").text();
-        console.log(img_src);
-        // debugger;
-        Tipped.delegate('.demo_tep',
-            "<div class='tipped-box'>" +
-            "<div class='title-box d-flex align-items-center'>" +
-            "<div class='img-box'>" +
-            "<img src='" + img_src + "' class='img-fluid' alt=''>" +
-            "</div>" +
-            "<div class='title-item-box'>" +
-            "<h4 class='title-item text-center'> " + box_title + "</h4>" +
-            "<p class='prag-item'> SoftBank provides fixed-line, mobile telephony, internet, " +
-            "<br> telecommunications, and digital television products.  " + "</p>" +
-            "<div class='list-item text-center'> <a href='#'> tag </a> <a href='#'> tag </a>  <a href='#'> tag </a> " + "</div>" +
-            "</div>" +
-            "</div>" +
-            "<div class='number-box'> " +
-            "<div class='number-item-box'> " +
-            "<span class='number-title-item'> Lorem </span>" +
-            "<span class='number-item'> 22 </span>" +
-            "</div>" +
-            "<div class='number-item-box'> " +
-            "<span class='number-title-item'> Lorem </span>" +
-            "<span class='number-item'> 22 </span>" +
-            "</div>" +
-            "</div>" +
-            "<hr>" +
-            "<div class='button-box'> " +
-            "<a href='#' class='button-item'>  <i class='fas fa-plus'></i> <span >save</span> </a>" +
-            "</div>" +
-            "</div>"
-
-            , {
-                // behavior: 'custom-slow',
-                skin: 'light',
-                // for test
-                close: true,
-
-            });
-    },
+    $(".data-label .item-box").hover(
+        function () {
+            // debugger;
+            var img_src = $(this).children(".img-box").children("img").attr("src"),
+                box_title = $(this).children(".item-name").text();
+            // console.log(img_src);
+            // debugger;
+            Tipped.delegate(
+                ".demo_tep",
+                "<div class='tipped-box'>" +
+                    "<div class='title-box d-flex align-items-center'>" +
+                    "<div class='img-box'>" +
+                    "<img src='" +
+                    img_src +
+                    "' class='img-fluid' alt=''>" +
+                    "</div>" +
+                    "<div class='title-item-box'>" +
+                    "<h4 class='title-item text-center'> " +
+                    box_title +
+                    "</h4>" +
+                    "<p class='prag-item'> SoftBank provides fixed-line, mobile telephony, internet, " +
+                    "<br> telecommunications, and digital television products.  " +
+                    "</p>" +
+                    "<div class='list-item text-center'> <a href='#'> tag </a> <a href='#'> tag </a>  <a href='#'> tag </a> " +
+                    "</div>" +
+                    "</div>" +
+                    "</div>" +
+                    "<div class='number-box'> " +
+                    "<div class='number-item-box'> " +
+                    "<span class='number-title-item'> Lorem </span>" +
+                    "<span class='number-item'> 22 </span>" +
+                    "</div>" +
+                    "<div class='number-item-box'> " +
+                    "<span class='number-title-item'> Lorem </span>" +
+                    "<span class='number-item'> 22 </span>" +
+                    "</div>" +
+                    "</div>" +
+                    "<hr>" +
+                    "<div class='button-box'> " +
+                    "<a href='#' class='button-item'>  <i class='fas fa-plus'></i> <span >save</span> </a>" +
+                    "</div>" +
+                    "</div>",
+                {
+                    // behavior: 'custom-slow',
+                    skin: "light",
+                    // for test
+                    close: true,
+                }
+            );
+        },
         function () {
             var img_src = "";
-            console.log(img_src);
-        });
-
-
-
-
+            // console.log(img_src);
+        }
+    );
 
     // search-input
     $(".search-input").keyup(function () {
         // debugger;
         var Companyvalue = $(this).val().toLowerCase();
         $(".add-popup .popup-box .item-continer-box .item-box li").filter(function () {
-
+            // debugger;
             if ($(this).toggle($(this).text().toLowerCase().indexOf(Companyvalue) > -1).hasClass("removed_data") !== true) {
+                // debugger;
                 $(this).toggle($(this).text().toLowerCase().indexOf(Companyvalue) > -1);
-                $(".list-item-controled.removed_data ").hide();
-            } if ($(this).toggle($(this).text().toLowerCase().indexOf(Companyvalue) > -1).hasClass("removed_data") == true) {
                 $(".list-item-controled.removed_data ").show();
+            }
+            if ($(this).toggle($(this).text().toLowerCase().indexOf(Companyvalue) > -1).hasClass("removed_data") == true) {
+                // debugger;
+                $(".list-item-controled.removed_data ").hide();
             }
         });
     });
-
-
-
-
-
 
     // datepicke
     $(".date_picker").datepicker();
@@ -90,19 +91,9 @@ $(document).ready(function () {
         $(".add-popup").fadeIn();
     });
 
-
-
-
-
-
-
     $("#tadetest thead .min_lable").each(function () {
         const tdLable = $(this).text(),
             datatable = $(this).data("table");
-
-
-
-
 
         if ($(this).hasClass("locked") == true) {
             var icone = " <i class='fa fa-lock' aria-hidden='true'></i> ",
@@ -112,59 +103,56 @@ $(document).ready(function () {
                 requred_input = "";
         }
 
-
-
-        $(".item-box ul.last-one").append('<li class="list-item-controled " data-list="' + datatable + '">' + "<h6 class='title-lable'>" + tdLable + "</h6>" + icone + "</li>");
-
         if ($(this).hasClass("show_lable") == true) {
-            var checked_Attr = "checked";
-
-
+            var checked_Attr = "checked",
+                added_class = "added_to_list",
+                removed_class = "";
         } else {
-            var checked_Attr = "";
-
-
-            $(".item-box ul.last-one .list-item-controled[data-list=" + datatable + "]").hide();
-
-
+            var checked_Attr = "",
+                added_class = "",
+                removed_class = "removed_data";
         }
+        $(".item-box ul.last-one").append('<li class="list-item-controled ' + removed_class + " " + added_class + ' " data-list="' + datatable + '">' + "<h6 class='title-lable'>" + tdLable + "</h6>" + icone + "</li>");
 
-
-
-
+        if ($(this).hasClass("show_lable") !== true) {
+            $(".item-box ul.last-one .list-item-controled[data-list=" + datatable + "]").hide();
+        }
 
         $(".item-box.sucond-box ul").append(
             " <li>" +
-            '<div class="pretty p-default">' +
-            '<input id="' +
-            datatable +
-            '" type="checkbox" class="input-checked" ' +
-            checked_Attr +
-            " " +
-            requred_input +
-            "/>" +
-            ' <div class="state p-primary p-svg">' +
-            " <label> " +
-            tdLable +
-            "</label>" +
-            " </div>" +
-            "    </div>" +
-            " </li>"
+                '<div class="pretty p-default">' +
+                '<input id="' +
+                datatable +
+                '" type="checkbox" class="input-checked" ' +
+                checked_Attr +
+                " " +
+                requred_input +
+                "/>" +
+                ' <div class="state p-primary p-svg">' +
+                " <label> " +
+                tdLable +
+                "</label>" +
+                " </div>" +
+                "    </div>" +
+                " </li>"
         );
     });
 
     $(".input-checked").click(function () {
         const id_attr = $(this).attr("id");
 
-        $("." + id_attr)
-            .toggleClass("show_lable")
-            .toggle();
+        // $("." + id_attr)
+        //     .toggleClass("show_lable")
+        //     .toggle();
 
-        $(".list-item-controled[data-list='" + id_attr + "']").toggle().toggleClass("removed_data");
+        $(".list-item-controled[data-list='" + id_attr + "']")
+            .toggle()
+            .toggleClass("added_to_list")
+            .toggleClass("removed_data");
     });
 
     $(".delet_icone_parrent").click(function () {
-        const name_attr = $(this).parent().attr("data-list");
+        var name_attr = $(this).parent().attr("data-list");
 
         $("#" + name_attr).click();
 
@@ -177,5 +165,36 @@ $(document).ready(function () {
 
     $(".closeandsave").click(function () {
         $(".add-popup").fadeOut();
+
+        const list_item = $(".add-popup .popup-box .item-continer-box .item-box .last-one li"),
+            lable_item = $("#tadetest thead .min_lable");
+
+        //   debugger;
+        for (var i = 0; i < list_item.length; i++) {
+            //   console.log(list_item[i]);
+
+            // debugger;
+            for (var j = 0; j < lable_item.length; j++) {
+                //   console.log(lable_item[j]);
+
+                // debugger;
+                if ($(list_item[i]).attr("data-list") == $(lable_item[j]).attr("data-table")) {
+                    //   debugger;
+                    if ($(list_item[i]).hasClass("removed_data") == true) {
+                        // debugger;
+                        var name_attr = $(list_item[i]).attr("data-list");
+                        $("." + name_attr)
+                            .removeClass("show_lable")
+                            .hide();
+                    } else if ($(list_item[i]).hasClass("added_to_list") == true) {
+                        //  debugger;
+                        var name_attr2 = $(list_item[i]).attr("data-list");
+                        $("." + name_attr2)
+                            .addClass("show_lable")
+                            .show();
+                    }
+                }
+            }
+        }
     });
 });
